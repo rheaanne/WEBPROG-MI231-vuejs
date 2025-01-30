00cfc202725e1310b562b8605596e5463b9a6a46
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-01-30 02:01:24 +0000

    update

--- vue-project/src/lib/supabaseclient.js
+++ vue-project/src/lib/supabaseclient.js
@@ -1,7 +1,3 @@
-// src/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://tarftmmhhbslgntaoxgp.supabase.co';
-const SUPABASE_ANON_KEY = 'your-anon-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhcmZ0bW1oaGJzbGdudGFveGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyODAsImV4cCI6MjA1MzU0NDI4MH0.k_d-qbr1Lv-FTsIMOucLHQUrY6PxMnnWl3w0txHSh6Y';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient('https://<project>.supahttps://tarftmmhhbslgntaoxgp.supabase.cobase.co', '<your-anoeyJhbGciOiJIUzI1eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhcmZ0bW1oaGJzbGdudGFveGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyODAsImV4cCI6MjA1MzU0NDI4MH0.k_d-qbr1Lv-FTsIMOucLHQUrY6PxMnnWl3w0txHSh6YNiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhcmZ0bW1oaGJzbGdudGFveGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyODAsImV4cCI6MjA1MzU0NDI4MH0.k_d-qbr1Lv-FTsIMOucLHQUrY6PxMnnWl3w0txHSh6Yn-key>')
