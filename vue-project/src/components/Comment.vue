a89c46edc4c4c83dfd61f11abc1b5f64ffe1d092
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-02-04 06:46:40 +0000

    update

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,9 +1,17 @@
 <template>
+  <div>
     <h1>Comments</h1>
     <ul>
-      <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
+      <li v-for="comment in comments" :key="comment.id">
+        {{ comment.content }} - By: {{ comment.profiles?.display_name || 'Unknown User' }}
+      </li>
     </ul>
-  </template>
+
+    <h2>Leave a Comment</h2>
+    <input type="text" v-model="commentText" placeholder="Your comment">
+    <button @click="submitComment">Submit</button>
+    <p v-if="submissionError">{{ submissionError }}</p> </div>
+</template>
   
   <script></script>
   
