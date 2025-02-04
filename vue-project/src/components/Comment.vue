ac9c45a23ccecf040c9e1a3193e0cde73e4a5223
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-02-04 14:31:32 +0000

    update

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,9 +1,4 @@
-<template>
-    <h1>Comments</h1>
-    <ul>
-      <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
-    </ul>
-  </template>
+
   
   <script></script>
   
