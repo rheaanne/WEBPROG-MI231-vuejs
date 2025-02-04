f5cb28c64c70a6861343427b2ed2b739d1d7faaf
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-02-04 06:54:59 +0000

    update

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,27 +1,4 @@
 <template>
-  <div>
-    <h1>Leave a Comment</h1>
-    <form @submit.prevent="submitComment">
-      <label>Name:</label>
-      <input v-model="name" type="text" required />
-      
-      <label>Comment:</label>
-      <textarea v-model="comment" required></textarea>
-      
-      <button type="submit">Submit</button>
-    </form>
-    
-    <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
-
-    <h1>Comments</h1>
-    <ul>
-      <li v-for="comment in comments" :key="comment.id">
-        <strong>{{ comment.name }}:</strong> {{ comment.comment }}
-      </li>
-    </ul>
-  </div>
-</template>
-
     <h1>Comments</h1>
     <ul>
       <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
