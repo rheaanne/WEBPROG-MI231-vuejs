95eaf3ef6c985a1b7121ba03e01222cf93a14e3a
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-02-04 14:04:45 +0000

    update

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,4 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@lib/supabaseClient'
+
 const supabaseUrl = 'https://tarftmmhhbslgntaoxgp.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
+
 const supabase = createClient(supabaseUrl, supabaseKey)
