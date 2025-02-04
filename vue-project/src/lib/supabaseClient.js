7486978a30e13ee040e7e26b17aa33cc3f379195
Rhea-Anne Danao <158535192+rheaanne@users.noreply.github.com>
2025-02-04 14:24:55 +0000

    update

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tarftmmhhbslgntaoxgp.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(
+  'https://tarftmmhhbslgntaoxgp.supabase.co', 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhcmZ0bW1oaGJzbGdudGFveGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyODAsImV4cCI6MjA1MzU0NDI4MH0.k_d-qbr1Lv-FTsIMOucLHQUrY6PxMnnWl3w0txHSh6Y'
+);
+
+
+export { supabase };
